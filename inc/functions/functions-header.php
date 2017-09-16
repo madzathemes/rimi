4c38ceea2b1cd6c0d222858b24a3f692bb48ef28
madzathemes <madza@madzathemes.com>
2017-09-16 20:49:05 +0300

    fix

--- inc/functions/functions-header.php
+++ inc/functions/functions-header.php
@@ -166,7 +166,7 @@ function rimi_logo() {
 		</a>
 	<?php } else { ?>
 		<a class="logo"  href="<?php echo esc_url(home_url('/'));?>">
-			<img src="<?php echo get_template_directory_uri(); ?>/inc/img/logo.png" width="170" height="52" alt="<?php echo the_title(); ?>" />
+			<img src="<?php echo get_template_directory_uri(); ?>/inc/img/logo.png" width="183" height="48" alt="<?php echo the_title(); ?>" />
 		</a>
 	<?php }
 }
