572e3b0b98004c248987bb3d7051ee07e7404425
madzathemes <madza@madzathemes.com>
2017-09-19 14:03:36 +0300

    fix

--- search.php
+++ search.php
@@ -23,7 +23,10 @@
 
 						<?php while ( have_posts() ) : the_post(); ?>
 							<h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'rimi' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php echo get_the_title();  ?></a></h2>
-						<?php endwhile; ?>
+						<?php endwhile;
+						the_posts_pagination();
+						?>
+
 					<?php else : ?>
 						<p><?php esc_html_e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'rimi'  ); ?></p>
 					<?php endif;
