4ad2fd440a19bb409bd64384f8b39e4c276b9976
madzathemes <madza@madzathemes.com>
2017-09-19 13:18:18 +0300

    # ------------------------ >8 ------------------------ # Do not touch the line above. # Everything below will be removed. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # Changes to be committed: # # 	modified:   search.php

--- search.php
+++ search.php
@@ -56,7 +56,7 @@
 						</div>
 						<?php if ( have_posts() ) : ?>
 
-							<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+							<?php while ( have_posts() ) : the_post(); ?>
 								<h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'rimi' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php echo get_the_title();  ?></a></h2>
 							<?php endwhile; ?>
 							<?php wp_reset_postdata(); ?>
