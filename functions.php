277226061ee7a118ae3731b6ddf28cec11278f13
madzathemes <madza@madzathemes.com>
2017-10-05 09:30:24 +0300

    fix

--- functions.php
+++ functions.php
@@ -36,34 +36,6 @@ require get_template_directory() .'/inc/single/single-styles.php';
 /*-----------------------------------------------------------------------------------*/
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
-function rimi_theme_setup() {
-
-	add_editor_style();
-	add_theme_support( 'post-formats', array('video', 'gallery') );
-	add_theme_support( 'automatic-feed-links' );
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'woocommerce' );
-	add_theme_support( "title-tag" );
-
-	load_theme_textdomain( 'rimi', get_template_directory() . '/languages' );
-	$locale = get_locale();
-	$locale_file = get_template_directory() . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
-
-	set_post_thumbnail_size( 999, 999, true );
-
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Header Menu', 'rimi' ),
-		'mobile' => esc_html__( 'Mobile Menu', 'rimi' ),
-		'footer_menu' => esc_html__( 'Footer Navigation', 'rimi' ),
-	) );
-	if ( ! isset( $content_width ) ) { $content_width = 900; }
-}
-
-add_action( 'after_setup_theme', 'rimi_theme_setup' );
-
-
 /*-----------------------------------------------------------------------------------*/
 /* Default Options
 /*-----------------------------------------------------------------------------------*/
